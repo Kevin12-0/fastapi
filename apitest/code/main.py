b980d86b3bb67ebd2e6f0194160ae0761b8b6618
Kevin12-0 <1719110155@utectulancingo.edu.mx>
2022-06-19 18:26:35 +0000

    cruds realizados

--- apitest/code/main.py
+++ apitest/code/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import *
 from typing import List
 from pydantic import BaseModel
 import sqlite3
@@ -12,39 +12,32 @@ class Cliente(BaseModel):
     nombre: str
     email: str
 
+class Registro(BaseModel):
+	nombre : str
+	email : str
+
 app = FastAPI()
 
 @app.get("/", response_model=Request)
 async def Index():
-    return {"message": 'Hola Mundo'}
+    return {"message": 'API REST'}
 
 @app.get('/clientes/',response_model=List[Cliente])
-async def Cliente():
+async def Cliente(offset:int = 0, limit:int = 3):
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM clientes')
+        cursor.execute('SELECT * FROM clientes LIMIT ? OFFSET ? ',(limit,offset))
         respose = cursor.fetchall()
         return respose
 
-'''
-    consulta por medio de metodo de cursor muestra todos los datos
-'''
-@app.get("/test/")
-async def fetch_data():
-    with sqlite3.connect('sql/clientes.sqlite') as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM clientes WHERE id_cliente = 1')
-        respose = cursor.fetchall()
-        return respose
 '''
     consulta pasando paramtros desde url a una base de datos
 '''
 @app.get("/consulta/{id}")
-async def fetch_data(id:int, q: Union[int, None] = None, short: bool = False):
+async def fetch_data(id:int):
     id_client = id
-    consulta = 'SELECT * FROM clientes WHERE id_cliente ='+str(id_client)
+    consulta = 'SELECT * FROM clientes WHERE id_cliente ={}'.format((int(id_client)))
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
@@ -52,15 +45,40 @@ async def fetch_data(id:int, q: Union[int, None] = None, short: bool = False):
         respose = cursor.fetchall()
         return respose
 '''
-    paso de paarametros por url
+	insertar registros en una tabla
 '''
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = item_id
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+@app.post('/insertar/{nombre}/{email}')
+async def Insertar(nombre:str,email:str):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('INSERT INTO clientes (nombre,email) VALUES (?,?)',(nombre,email))
+		cursor.fetchall()
+		return {"mensaje":"Cliente agregado"}
+
+'''
+	Actualizar registros de una tabla
+'''
+@app.put('/actulizar/{id}/{nombre}/{email}')
+async def actulizar(id:str, nombre:str, email:str):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('UPDATE clientes SET nombre = ?, email = ? WHERE id_cliente = ?',(nombre,email,id))
+		cursor.fetchall()
+		return {"mensaje":"Cliente Actualizado"}
+'''
+	Eliminar un registro
+'''
+@app.delete('/eliminar/{id}')
+async def eliminar(id:int):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('DELETE FROM clientes WHERE id_cliente= {}'.format(int(id)))
+		cursor.fetchall()
+		return {"mensaje":"Cliente borrado"}
+
+
+
+
